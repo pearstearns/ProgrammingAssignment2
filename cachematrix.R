69c8cd865b4ed240f7f9d3cd07771335f0e7ab9a
pearstearns <ikroberts1992@gmail.com>
2016-06-18 14:06:04 -0400

    ProgrammingAssignment2 (Submitted early)
    mainly some grammar changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
-## this isn't a function itself so much as a list of functions, kind of like a case of DVDs. 
-##you can't put the big blue case in the player and expect it to play, 
+##This isn't a function itself so much as a list of functions, kind of like a case of DVDs. 
+##You can't put the big blue case in the player and expect it to play, 
 ##but you can put Speed Racer (2008) dirs. Wachowski Sisters, 
 ##(bigblue$SpeedRacer)
 ##into the machine and it will play just fine.
-## so basically this has the $set, $get, $setinv and $getinv functions
-## you put in a matrix (I use rnorm as well, they need to be square)
-## and it puts that in to the (x) variable within the function
-## get reads x
-## setinv takes x and solves it, placing the inverse matrix in the m variable
-## getinv reads m
+##So basically this has the $set, $get, $setinv and $getinv functions
+##You put in a matrix (I use rnorm as well, they need to be square) and $set it puts that in to the (x) variable within the function
+## $get reads x
+## $setinv takes x and solves it, placing the inverse matrix in the m variable
+## $getinv reads m
 
 makeCacheMatrix <- function(x = matrix()) {
 ## A list of functions to set, retrieve, and invert matrices        
@@ -29,7 +28,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The main function is to return a matric that is the inverse of 'x'. 
+## The main function is to return a matrix that is the inverse of 'x' using the previous makeCacheMatrix 
 ## it does this in one of two ways
 ## 1) cacheSolve checks $getinv to see if there is already an inverse matrix there
 ## 2) if it isn't it takes the $get value and runs solve() on it.
